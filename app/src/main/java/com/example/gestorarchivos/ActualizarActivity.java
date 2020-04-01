aab89561e50ffcead45debbd66ed9d0c2505bdb0
Mgomez <mateogomezramirez@hotmail.com>
2020-04-01 00:04:05 -0500

    Se creo layout del dispositivo

---
+++ app/src/main/java/com/example/gestorarchivos/ActualizarActivity.java
@@ -0,0 +1,14 @@
+package com.example.gestorarchivos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActualizarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar);
+    }
+}
